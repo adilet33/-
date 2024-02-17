7842474528b6bcf3d5f055dc1c198733d9c81f9a
AdiletAshimov <100845836+adilet33@users.noreply.github.com>
2024-02-18 00:06:30 +0600

    Update views.py

--- graduates/views.py
+++ graduates/views.py
@@ -7,3 +7,10 @@ def graduate_list(request):
     return render(request, 'graduates/gradueate_list.html', {'graduates': graduates})
 
 
+
+def graduate_detail(request, graduate_id):
+    graduate = get_object_or_404(Graduate, pk=graduate_id)
+    return render(request, 'graduates/graduate_detail.html', {'graduate': graduate})
+
+
+
